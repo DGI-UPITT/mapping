24c5e699faf3053b212134457a2a419797cab5a0
Danny Joris <danny_joris@yahoo.co.uk>
2012-03-29 15:53:26 -0300

    added 4 zoom levels with different images

--- js/mapping.js
+++ js/mapping.js
@@ -40,20 +40,57 @@ function initialize_map() {
   // set map
   map = new google.maps.Map(document.getElementById('map-test'), opts);
 
-//  // adds to the marker bound
-//  bounds.extend(imageBounds);
-//  //bounds.extend(40.765641,-74.139235);
-
+  //  // adds to the marker bound
+  var oldmap;
   var oldmap = new google.maps.GroundOverlay(
       "http://d6theming.local/sites/all/modules/custom/mapping/images/upitt-map2500.png",
       imageBounds);
+  
+  // add image to the map
   oldmap.setMap(map);
-
+  
+  // set opacity
+  oldmap.setOpacity(0.5);
+  
   // fit the bounds we created
   map.fitBounds(imageBounds);
-
-// 39v02p01-011_Clip1.tiff
-
-
   
+
+  // 39v02p01-011_Clip1.tiff
+
+
+  var test;
+  var zoomLevel = 1;
+
+  google.maps.event.addListener(map, 'zoom_changed', function(event) {
+    var i, prevZoomLevel;
+
+    if (map.getZoom() === 16) {
+      oldmap.setMap();
+      oldmap.url = "http://d6theming.local/sites/all/modules/custom/mapping/images/upitt-map2500.png";
+      oldmap.setMap(map);
+    }
+    if (map.getZoom() === 17) {
+      // unset image object from the map
+      oldmap.setMap();
+      // change image url
+      oldmap.url = "http://d6theming.local/sites/all/modules/custom/mapping/images/upitt-map2500-2.png";
+      // set image to the map again
+      oldmap.setMap(map);
+    }
+    if (map.getZoom() === 18) {
+      oldmap.setMap();
+      oldmap.url = "http://d6theming.local/sites/all/modules/custom/mapping/images/upitt-map2500-3.png";
+      oldmap.setMap(map);
+    }
+    if (map.getZoom() === 19) {
+      oldmap.setMap();
+      oldmap.url = "http://d6theming.local/sites/all/modules/custom/mapping/images/upitt-map2500-4.png";
+      oldmap.setMap(map);
+    }
+
+
+
+  });
+
 }
